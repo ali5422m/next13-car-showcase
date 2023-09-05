7fa93fff1070c8004bc77a5d3c9821d569883219
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-09-05 23:20:13 +0330

    create structure page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
             </div>
             <div className="hero__image-container">
                 <div className="hero__image">
-                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
+                    <Image src="/lexus.png" alt="hero" fill className="object-contain" />
                 </div>
 
                 <div className="hero__image-overlay" />
